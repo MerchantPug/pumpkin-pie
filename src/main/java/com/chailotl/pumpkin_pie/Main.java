8eb7925932a8091556a484f7ddb9fef62a328ca8
MerchantPug <merchantpug@gmail.com>
2025-03-20 09:29:50 +1100

    Fix usage of Porting Lib, update to Refabricated 3.0.0.

--- src/main/java/com/chailotl/pumpkin_pie/Main.java
+++ src/main/java/com/chailotl/pumpkin_pie/Main.java
@@ -4,15 +4,20 @@ import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
 import net.minecraft.block.*;
 import net.minecraft.item.*;
+import net.minecraft.loot.LootTable;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import vectorwing.farmersdelight.FarmersDelight;
 import vectorwing.farmersdelight.common.FoodValues;
 import vectorwing.farmersdelight.common.registry.ModCreativeTabs;
 import vectorwing.farmersdelight.common.registry.ModItems;
+import vectorwing.farmersdelight.refabricated.LootModificationEvents;
 
 import static vectorwing.farmersdelight.common.registry.ModItems.*;
 
@@ -21,6 +26,8 @@ public class Main implements ModInitializer
 	public static final Item PUMPKIN_PIE_SLICE;
 	public static final Block PUMPKIN_PIE;
 	public static final Item PUMPKIN_PIE_ITEM;
+
+	private static final RegistryKey<LootTable> BLOCKS_PUMPKIN_PIE = RegistryKey.of(RegistryKeys.LOOT_TABLE, Identifier.of(FarmersDelight.MODID, "blocks/pumpkin_pie"));
 
 	static
 	{
@@ -32,6 +39,11 @@ public class Main implements ModInitializer
 	@Override
 	public void onInitialize() {
 		ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.put(PUMPKIN_PIE_SLICE, 0.85F);
+
+		LootTableEvents.MODIFY.register((key, tableBuilder, source, registries) -> {
+			if (key == BLOCKS_PUMPKIN_PIE)
+				LootModificationEvents.pastrySlicing(tableBuilder, PUMPKIN_PIE, PUMPKIN_PIE_SLICE, PumpkinPieBlock.BITES, 4);
+		});
 
 		ServerLifecycleEvents.SERVER_STARTING.register(server -> {
 			var key = Registries.ITEM_GROUP.getKey(ModCreativeTabs.TAB_FARMERS_DELIGHT.get()).get();
